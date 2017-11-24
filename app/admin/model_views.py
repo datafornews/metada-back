c01c1754ffd1a1724df9e3a3983fe27d11d78327
vict0rsch <vsch@protonmail.com>
2017-11-24 10:44:17 +0100

    role dependent model view

--- app/admin/model_views.py
+++ app/admin/model_views.py
@@ -2,12 +2,14 @@ import datetime
 from flask_admin import helpers as admin_helpers
 from flask_admin.contrib.sqla import ModelView, fields
 from flask_admin.form.fields import Select2Field
+from flask_admin.contrib.sqla.filters import BooleanEqualFilter
 
 from flask_security import Security, SQLAlchemyUserDatastore, current_user
 from wtforms.fields import PasswordField
 from sqlalchemy_utils import Choice
 
 from flask import url_for, redirect, request, abort
+from flask import has_app_context
 
 
 class SelectField(Select2Field):
@@ -62,7 +64,45 @@ class SafeModelView(ModelView):
                 return redirect(url_for('security.login', next=request.url))
 
 
-class EntityModelView(SafeModelView):
+class RoleSafeModelView(SafeModelView):
+    @property
+    def column_list(self):
+        if has_app_context() and current_user.has_role('superuser'):
+            return self.superuser_column_list
+        return self.moderator_column_list
+
+    @property
+    def _list_columns(self):
+        return self.get_list_columns()
+
+    @_list_columns.setter
+    def _list_columns(self, value):
+        pass
+
+    @property
+    def form_columns(self):
+        if has_app_context() and current_user.has_role('superuser'):
+            return self.superuser_form_columns
+        return self.moderator_form_columns
+
+    @property
+    def _create_form_class(self):
+        return self.get_create_form()
+
+    @_create_form_class.setter
+    def _create_form_class(self, value):
+        pass
+
+    @property
+    def _edit_form_class(self):
+        return self.get_edit_form()
+
+    @_edit_form_class.setter
+    def _edit_form_class(self, value):
+        pass
+
+
+class EntityModelView(RoleSafeModelView):
     ownership = [
         ('c', 'company'),
         ('i', 'individual'),
@@ -72,21 +112,20 @@ class EntityModelView(SafeModelView):
     # column_searchable_list = ['name', 'wiki.title', 'id']
     column_searchable_list = ['name', 'id']
 
-    column_list = ['name', 'website', 'wiki_link', 'long_name',
-                   'other_groups', 'category', 'id',
-                   'updated_by', 'updated_at', 'created_by', 'created_at', # 'wiki',
-                   ]
-    # column_list = ['name', 'website', 'wiki_link', 'long_name',
-    #                'other_groups', 'category', 'id']
-    column_sortable_list = ['name', 'id']
+    superuser_column_list = ['name', 'website', 'wiki_link', 'long_name',
+                             'other_groups', 'category', 'id',
+                             'updated_by', 'updated_at', 'created_by', 'created_at',  # 'wiki',
+                             ]
+    moderator_column_list = ['name', 'website', 'wiki_link', 'long_name',
+                             'other_groups', 'category', 'id']
+    superuser_form_columns = superuser_column_list
+    moderator_form_columns = moderator_column_list
 
+    column_sortable_list = ['name', 'id']
     column_editable_list = ['name', 'website', 'wiki_link',
                             'other_groups', 'long_name']
 
-    form_columns = ['name', 'category', 'website',
-                    'wiki_link', 'long_name', 'other_groups']
     form_overrides = dict(category=SelectForChoiceTypeField)
-
     form_args = {
         'category': {
             'choices': ownership,
@@ -106,6 +145,15 @@ class WikiDataModelView(SafeModelView):
     column_searchable_list = ['title', 'lang', 'entity.name']
     column_list = ['title', 'lang', 'entity.name']
     column_editable_list = ['title', 'lang']
+
+    def is_accessible(self):
+        if not current_user.is_active or not current_user.is_authenticated:
+            return False
+
+        if current_user.has_role('superuser'):
+            return True
+
+        return False
 
 
 class UserModelView(SafeModelView):
@@ -149,8 +197,15 @@ class UserModelView(SafeModelView):
         return False
 
 
-class EdgeModelView(SafeModelView):
+class EdgeModelView(RoleSafeModelView):
     column_searchable_list = ['child.name', 'parent.name', 'value']
+
+    superuser_column_list = ['parent', 'child', 'value',
+                             'updated_by', 'updated_at', 'created_by', 'created_at']
+    superuser_form_columns = superuser_column_list
+
+    moderator_column_list = ['parent', 'child', 'value']
+    moderator_form_columns = moderator_column_list
 
     def on_model_change(self, form, edge, is_created):
         now = datetime.datetime.now()
